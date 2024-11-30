00bd9b115e4b66602e90abd9850439113bd65ab5
gabisaCSS <gtrmastah@gmail.com>
2024-11-30 19:09:30 +0700

    feat: connect with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,13 @@ import 'package:auth/presentation/pages/splash_screen.dart';
 import 'package:core/presentation/blocs/homepage_bloc/homepage_bloc.dart';
 import 'package:core/presentation/pages/homepage.dart';
 import 'package:core/utils/route_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:core/utils/routes.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:profile/presentations/blocs/profile_bloc/profile_bloc.dart';
+import 'package:telkom_teknisi_app/firebase_options.dart';
 import 'package:telkom_teknisi_app/injection.dart' as di;
 import 'package:ticket/presentation/blocs/active_ticket_bloc/active_ticket_bloc.dart';
 import 'package:ticket/presentation/blocs/historic_ticket_bloc/historic_ticket_bloc.dart';
@@ -18,6 +20,9 @@ import 'package:ticket/presentation/pages/detail_ticket_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await initializeDateFormatting('id_ID', null);
   await di.init(); // dependency injections
   runApp(const MyApp());
